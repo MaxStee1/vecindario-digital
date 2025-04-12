8a244a59dd6dd2e4db7a34a41804029bd13d41b8
MaxSteel <maxm2004.mc@gmail.com>
2025-04-12 19:28:21 -0400

    eliminar componentes no utilizados

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import styles from "../styles/homePage.module.css";
 
 
 const LoginPage = () => {
